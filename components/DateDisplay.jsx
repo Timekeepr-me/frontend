127e3c96bc42938dc1ebeafbfaaa46e21ee67c33
Mike Starr <imhs@protonmail.com>
2022-09-16 22:17:19 -0400

    Added form for date select

---
+++ components/DateDisplay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { date } from "../constants/date";
+
+const DateDisplay = () => {
+  // console.log(date);
+  return <div className="text-white"></div>;
+};
+
+export default DateDisplay;
