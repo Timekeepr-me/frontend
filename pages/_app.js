ec4c0b9d50e06e103359cab418a904f018d4ec3b
Shayan S <91401566+sha256yan@users.noreply.github.com>
2022-09-21 15:07:49 +1000

    added skeleton for XmtpContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import { WalletProvider } from "../context/WalletContext";
 import { DateProvider } from "../context/DateContext";
+import { XmtpClient } from "../context/XmtpContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <WalletProvider>
       <DateProvider>
+        <XmtpClient></XmtpClient>
         <Component {...pageProps} />
       </DateProvider>
     </WalletProvider>
