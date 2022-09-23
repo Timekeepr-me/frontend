aa6704a1ea085d54ccd5fb6d6f68d32163b90638
Mike Starr <imhs@protonmail.com>
2022-09-22 23:03:35 -0400

    Added Modal. Started buidling BookingForm. Changes to landing page

--- pages/calendar.js
+++ pages/calendar.js
@@ -6,7 +6,7 @@ import Month from "../components/Month";
 import Button from "../components/Button";
 import Footer from "../components/Footer";
 import { DateContext } from "../context/DateContext";
-import Modal from "../components/Modal";
+// import Modal from "../components/Modal";
 
 const Calendar = () => {
   const context = useContext(DateContext);
@@ -52,7 +52,7 @@ const Calendar = () => {
           <div>
             <Button text="Month" click={handleMonthBtn} />
           </div>
-          <Modal />
+          {/* <Modal /> */}
         </div>
         <div className="col-span-7">{renderRange()}</div>
       </div>
